31b267ce607232f7e9a0da00bd88c561b30c87da
msetu1 <msetu5763@gmail.com>
2025-02-19 16:33:31 +0600

    error conflig fixed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const tourismCollection = client.db("touristsProject");
     const tourists = tourismCollection.collection("tourists");
@@ -119,7 +119,7 @@ app.delete('/delete/:id',async(req,res)=>{
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
